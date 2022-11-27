23a98f3d9eb45c3bbb0a7d17b2783cb0b5f7adb5
Mykaelsss <mykaelsicard04@gmail.com>
2022-11-26 19:36:21 -0500

    Initial Commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Mykael Sicard");
+        System.out.println("I am 18 years old");
+        System.out.println("My hometown is Bronx, NY");
+    }
+}
